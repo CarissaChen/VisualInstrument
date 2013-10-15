e332bb28ed1360b3fec739285477549008a4c64a
Brian Newsom <brian.newsom@colorado.edu>
2013-10-15 17:59:37 -0600

    first push

---
+++ VI4/src/main/java/com/hccpd/vi4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hccpd.vi4;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class MainActivity extends Activity {
+    private DrawingView drawView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get drawing view
+        drawView = (DrawingView)findViewById(R.id.drawing);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
